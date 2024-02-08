773da10486f11fc57ee251b4cc1ed12911c89adc
Philip Thomson <pst16@georgetown.edu>
2024-02-08 12:33:49 -0500

    Added Meyer Reset  and updated game objects
    Created game tile object and updated game object behavior

--- js/script.js
+++ js/script.js
@@ -7,12 +7,15 @@ function createGameController() {
     const gameBoard = createGameBoard();
     const player1 = createPlayer("Player 1");
     const player2 = createPlayer("Player 2");
+
+    gameBoard.resetBoard();
 }
 
 function createGameBoard() {
 
     const boardContainer = document.querySelector("hey");
     const boardValues = [];
+    const tiles = [];
 
     const resetBoard = () => {
 
@@ -21,12 +24,38 @@ function createGameBoard() {
         for(let i = 0; i < 9; i++) {
 
             boardValues.push("empty");
+            tiles.push(createTile());
         }
+
+        updateVisual();
     };
 
     const updateVisual = () => {
 
+        for(let i = 0; i < 9; i++) {
+
+            tiles[i].setVisual();
+        }
     };
+
+    return {resetBoard, updateVisual};
+}
+
+function createTile() {
+
+    const setVisual = (value) => {
+
+        switch(value) {
+            case "X":
+                break;
+            case "O":
+                break;
+            default:
+                break;
+        }
+    };
+
+    return { setVisual };
 }
 
 function createPlayer(name) {
